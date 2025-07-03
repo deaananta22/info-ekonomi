b00f63838b8ae4028025d6812dfd7eb7e64668fd
fadillah <fadillahd544@gmail.com>
2025-07-03 11:12:42 +0700

    dila database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KepalaController;
+use App\Http\Controllers\KIController;
+use App\Http\Controllers\TimController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Kepala
+Route::middleware(['auth', 'role:kepala'])->group(function () {
+    Route::get('/kepala/dashboard', [KepalaController::class, 'index']);
+});
+
+// KI
+Route::middleware(['auth', 'role:ki'])->group(function () {
+    Route::get('/ki/dashboard', [KIController::class, 'index']);
+});
+
+// Tim
+Route::middleware(['auth', 'role:tim'])->group(function () {
+    Route::get('/tim/dashboard', [TimController::class, 'index']);
 });
